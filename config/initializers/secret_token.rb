c82bd0091a4334ae30bcdfd0ef146a6e2ebd8121
ixnee <balogh23@gmail.com>
2014-09-27 20:38:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaloghTodo01::Application.config.secret_token = 'f943ecc502e0259bf3db5d4731710b5986cdfaf96d9cef96810e79212873a738a566d287f69802a546adf7f9b2a421eaa261f113e8b2b9e04fb6da4419d64343'
